4e243636c9eb068d1033f3694a7b0fc7095d42a4
josedelphin <dalejandro.005@gmail.com>
2023-01-10 20:54:46 -0400

    Add Mobile Version witn Menu Hamburguer

--- hamburguer.js
+++ hamburguer.js
@@ -1,1 +1,21 @@
-console.log('Hola Mundo');
+const btnClose = document.querySelector('#btn-close');
+const divHamburguer = document.querySelector('.hamburguer');
+const iconHamburguer = document.querySelector('#hamburguer-icon');
+const hamburguerLink = document.querySelectorAll('.hamburguer-link');
+
+btnClose.addEventListener('click', () => {
+  divHamburguer.classList.add('run-out');
+  divHamburguer.classList.remove('run');
+});
+
+iconHamburguer.addEventListener('click', () => {
+  divHamburguer.classList.add('run');
+  divHamburguer.classList.remove('run-out');
+});
+
+for (let i = 0; i < hamburguerLink.length; i += 1) {
+  hamburguerLink[i].addEventListener('click', () => {
+    divHamburguer.classList.add('run-out');
+    divHamburguer.classList.remove('run');
+  });
+}
